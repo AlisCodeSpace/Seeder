35dd4b474378a2e5d2a881d1202656474f25aacd
Ali Anani <a.anani@mrad-services.com>
2025-01-07 16:55:56 +0200

    Update Home component padding for improved layout consistency

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Navbar from "../components/global/Navbar"
 const Home = () => {
   return (
     <div className="relative container">
-      <div className="mb-24">
+      <div className="pb-24">
         <Outlet />
       </div>
       <Navbar />
