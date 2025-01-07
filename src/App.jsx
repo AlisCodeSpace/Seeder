71ce169677bfa8215f457db88c8cdd67589eb493
Ali Anani <a.anani@mrad-services.com>
2025-01-07 20:25:06 +0200

    Add NextUI styles for improved component styling and layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import './styles/input.css'
 import './styles/links.css'
 import './styles/animations.css'
+import './styles/nextui.css'
 
 
 import AppRoutes from "./routes/AppRoutes"; 
